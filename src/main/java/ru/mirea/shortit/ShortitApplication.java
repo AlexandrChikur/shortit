0bed37b1ef8d61ae150faf33830b413c004fc23b
AlexandrChikur <sasha.chikur@mail.ru>
2021-05-08 02:13:35 +0300

    init

---
+++ src/main/java/ru/mirea/shortit/ShortitApplication.java
@@ -0,0 +1,13 @@
+package ru.mirea.shortit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mirea/shortit/ShortitApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.shortit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
